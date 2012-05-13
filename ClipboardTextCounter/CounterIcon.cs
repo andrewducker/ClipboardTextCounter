413b1e8611bfcd51572a31228c3864da5dcc6cc4
Andrew Ducker <andrew@ducker.org.uk>
2012-05-13 20:53:02 +0100

    First checkin

---
+++ ClipboardTextCounter/CounterIcon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClipboardTextCounter
+{
+    //Code based on http://alanbondo.wordpress.com/2008/06/22/creating-a-system-tray-app-with-c/
+    public class CounterIcon : Form
+    {
+        private readonly NotifyIcon notifyIcon = new NotifyIcon();
+        private readonly ContextMenu menu = new ContextMenu();
+
+        private GlobalHotkey hotKey;
+
+        public CounterIcon()
+        {
+            menu.MenuItems.Add("Count", delegate { CountText(); });
+            menu.MenuItems.Add("Exit", delegate { Application.Exit(); });
+            notifyIcon.Text = "Clipboard Text Counter";
+            notifyIcon.Icon = new Icon(SystemIcons.Question, 40, 40);
+            notifyIcon.ContextMenu = menu;
+            notifyIcon.Visible = true;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == GlobalHotkey.WM_HOTKEY_MSG_ID)
+            {
+                CountText();
+            }
+            base.WndProc(ref m);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+            //This must be set under ShowInTaskbar, as that changes the hWnd of the form.
+            hotKey = new GlobalHotkey(GlobalHotkey.WIN, Keys.C, this);
+            base.OnLoad(e);
+        }
+
+        private static void CountText()
+        {
+            string clipText = Clipboard.ContainsText() ? Clipboard.GetText().Length.ToString() : "Clipboard does not contain text.";
+            MessageBox.Show(clipText);
+        }
+
+        private bool disposed;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+            disposed = true;
+            if (disposing)
+            {
+                notifyIcon.Dispose();
+                hotKey.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ClipboardTextCounter/GlobalHotkey.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ClipboardTextCounter
+{
+    //Code based on http://www.dreamincode.net/forums/topic/180436-global-hotkeys/
+    class GlobalHotkey : IDisposable
+    {
+        //modifiers
+        // ReSharper disable UnusedMember.Global
+        public const int NOMOD = 0x0000;
+        public const int ALT = 0x0001;
+        public const int CTRL = 0x0002;
+        public const int SHIFT = 0x0004;
+        public const int WIN = 0x0008;
+        // ReSharper restore UnusedMember.Global
+
+        //windows message id for hotkey
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        [DllImport("kernel32.dll")]
+        private static extern short GlobalAddAtom(string atomName);
+        [DllImport("kernel32.dll")]
+        private static extern short GlobalDeleteAtom(short atom);
+
+        private readonly IntPtr hWnd;
+        private readonly short id;
+
+        public GlobalHotkey(int modifier, Keys keys, IWin32Window form)
+        {
+            hWnd = form.Handle;
+            id = GlobalAddAtom("Hotkey" + modifier + keys + form.Handle);
+            RegisterHotKey(form.Handle, id, modifier, (int)keys);
+        }
+
+        private bool disposed;
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                disposed = true;
+                GlobalDeleteAtom(id);
+                UnregisterHotKey(hWnd, id);
+            }
+        }
+
+        ~GlobalHotkey()
+        {
+            Dispose();
+        }
+    }
+}
